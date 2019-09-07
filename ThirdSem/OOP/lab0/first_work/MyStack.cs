e1c42e1a0175efd56cf4ee00b36d308240466d21
Kircatle <krillsmain@yandex.ru>
2019-09-07 11:24:13 +0300

    add 0 lab oop

---
+++ ThirdSem/OOP/lab0/first_work/MyStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_work
+{
+    class MyStack
+    {
+        public int size_stack;
+        public int[] nums;
+        public int last_elem;
+
+        public MyStack(int size_stack)
+        {
+            this.size_stack = size_stack;
+            this.nums = new int[this.size_stack];
+            this.last_elem = -1;
+        }
+        public void add_to_stack(int elem)
+        {
+            if (this.last_elem == this.size_stack - 1)
+            {
+                Console.WriteLine("Ошибка: Превышен размер стека");
+                return;
+            }
+            this.last_elem++;
+            this.nums[this.last_elem] = elem;
+        }
+        public void pop_from_stack()
+        {
+            if (this.last_elem == -1)
+            {
+                Console.WriteLine("Ошибка: Стек пуст");
+                return;
+            }
+            this.nums[this.last_elem] = 0;
+            this.last_elem--;
+        }
+        public int top_elem()
+        {
+            if (this.last_elem == -1)
+            {
+                Console.WriteLine("Ошибка: Стек пуст");
+                return -1;
+
+            }
+
+            return this.nums[this.last_elem];
+        }
+        public int count_elem()
+        {
+            return this.last_elem + 1;
+        }
+    }
+}
---
+++ ThirdSem/OOP/lab0/first_work/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("first_work")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("first_work")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("81b6bbfe-2bf5-4684-9ae6-818ec19ab8b6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
